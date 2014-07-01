e448ff43d45a68df136171f9b8661d5d69b91c88
itisioslab <itisioslab@Mac-mini-administrator-4.local>
2014-07-01 10:11:14 +0400

    Initial Commit

---
+++ MyProductTests/MyProductTests.m
@@ -0,0 +1,34 @@
+//
+//  MyProductTests.m
+//  MyProductTests
+//
+//  Created by itisioslab on 01.07.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyProductTests : XCTestCase
+
+@end
+
+@implementation MyProductTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
